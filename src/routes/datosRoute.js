1f385090c3e767aa114c28c1f891be4086034330
Aaron8027 <77461333+Aaron8027@users.noreply.github.com>
2022-02-05 20:09:30 -0600

    Guardar en la db async y await
    Corrección 2.0

--- src/routes/datosRoute.js
+++ src/routes/datosRoute.js
@@ -1,6 +1,8 @@
 const{Router} = require('express');
 const router = Router();
 
+
+
 const modelDatos = require('../models/datosGenerales');
 
 router.get('/', async (require,res)=>{
@@ -9,12 +11,14 @@ router.get('/', async (require,res)=>{
     //res.send("consulta realizada");
 });
 
+
+
 router.post('/', async (require,res)=>{
     const {nombre, a_Paterno, a_Materno, celular, sexo, dia, mes, año, email, contraseña} = require.body;
     const newDato = new modelDatos({nombre, a_Paterno, a_Materno, celular, sexo, dia, mes, año, email, contraseña});
     console.log(newDato);
 
-    await newDato.save(); //guardar en la db async y await hacen que las cosas se hagan al mismo tiempo
+    await newDato.save(); 
     //console.log(require.body);
     res.json(newDato);
    
